f6557086d0d66d2323f73353d254eb2903b25265
ericsimonlarios <larios.ericsimon.122@gmail.com>
2022-03-16 19:19:50 +0800

    NavBar

--- 03_PHP_WITH_SIMON/navigation.php
+++ 03_PHP_WITH_SIMON/navigation.php
@@ -5,14 +5,29 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/index.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
     <title>Document</title>
 </head>
 
 <body>
-    <nav class="navbar">
-        <a class="" href="">Ornamentals Fish</a>
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="navbar-wrapper">
+            <!-- Logo -->
+            <img src="images/simon.png" class="nav-logo" alt="">
+            <ul class="nav-list">
+                <li><a class="nav-text" href="">Home</a></li>
+                <li><a class="nav-text" href="">Events</a></li>
+                <li><a class="nav-text" href="">Announcements</a></li>
+                <li><a class="nav-text" href="">Store</a></li>
+                <li><a class="nav-text" href="">Producers</a></li>
+                <li><a class="nav-text" href="">Courses</a></li>
+                <li><a class="nav-text" href="">Contacts</a></li>
+                
+            </ul>
+            <div class="btn btn-light login-btn">Login</div>
+        </div>
+     
     </nav>
 </body>
 
