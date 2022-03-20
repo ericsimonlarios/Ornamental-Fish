0384984122522529daaeca63437d435e5a7ef21e
ericsimonlarios <larios.ericsimon.122@gmail.com>
2022-03-20 10:52:58 +0800

    Changes

--- 03_PHP_WITH_SIMON/navigation.php
+++ 03_PHP_WITH_SIMON/navigation.php
@@ -7,14 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/index.css">
-    <title>Document</title>
+    <title>Filipinas Betta Ornamental Club</title>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="navbar-wrapper">
             <!-- Logo -->
-            <img src="images/simon.png" class="nav-logo" alt="">
+            <img src="images/fboc-logo.jpg" class="nav-logo" alt="">
             <ul class="nav-list">
                 <li><a class="nav-text" href="">Home</a></li>
                 <li><a class="nav-text" href="">Events</a></li>
