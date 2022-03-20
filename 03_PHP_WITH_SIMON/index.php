482c85a0d21fab4f203cb5b57af7139569e65a97
ericsimonlarios <larios.ericsimon.122@gmail.com>
2022-03-20 12:06:16 +0800

    Changes

--- 03_PHP_WITH_SIMON/index.php
+++ 03_PHP_WITH_SIMON/index.php
@@ -6,17 +6,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/index.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>index</title>
 </head>
 
 <body>
+    <?php include 'navigation.php' ?>
     <div class="container1">
         <div class="try">
-            <video autoplay muted loop id="MyVideo">
-                <source src="images/fish-background.mp4" type="video/mp4">
-            </video>
+            <div class="video-container"> 
+                <video autoplay muted loop id="MyVideo">
+                    <source src="images/fish-background.mp4" type="video/mp4">
+                </video>
+            </div>
             <div class="paper">
                 <div class="content">
                     <span class="title-1">Ornamental Fishes</span><br>
@@ -26,8 +28,7 @@
         </div>
     </div>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
 </script>
 
 </html>
