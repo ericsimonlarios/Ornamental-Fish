f98e6a677243dfa2f950257209740067b63b0b18
ericsimonlarios <larios.ericsimon.122@gmail.com>
2022-03-20 15:59:33 +0800

    changes

--- 03_PHP_WITH_SIMON/index.php
+++ 03_PHP_WITH_SIMON/index.php
@@ -21,11 +21,14 @@
             </div>
             <div class="paper">
                 <div class="content">
-                    <span class="title-1">Ornamental Fishes</span><br>
+                    <span class="title-1">Filipinas Betta Ornamental Club</span><br>
                     <span class="sentence">Meet the latest news of the Market.</span>
                 </div>
             </div>
         </div>
+    </div>
+    <div>
+        class
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
